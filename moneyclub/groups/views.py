ebf2adcd72cb4686eec567f4894308ee3470d293
amey <gamoholic010@gmail.com>
2013-11-11 02:07:21 -0500

    adduser to group

--- moneyclub/groups/views.py
+++ moneyclub/groups/views.py
@@ -34,7 +34,7 @@ def club_home(request):
     g = Group(name=name,owner=owner,description=description,keywords=keywords)
     g.save()"""
     
-    g=Group.objects.get(id=6);
+    g=Group.objects.get(id=1);
     context['group_name'] = g.name
     context['description'] = g.description
     str1= g.keywords
@@ -91,8 +91,44 @@ def club_create_submit(request):
     context['id']=g.id
     return render(request, 'moneyclub/group_home_page.html', context)
 
-def add_members(request,id):
-    return render(request, 'moneyclub/adduser.html', {})
+@transaction.commit_on_success
+def add_members_generic(request):
+    context={}
+    grp = Group.objects.filter(owner=request.user)
+    if grp.count()>0:
+        #if user owns some group
+        #get all groups which user owns
+        context['group_ids']=grp
+        groups = Group.objects.filter(owner=request.user)
+        context['groups']=groups
+        return render(request, 'moneyclub/adduser.html', context)
+    else:
+        club_create(request)
+@transaction.commit_on_success   
+def add_members(request):
     
+    if not 'email' in request.POST or not request.POST['email'] or request.method=="GET":
+        add_members_generic(request)
+        
+    #get group name from group id
+    grp_name=Group.objects.get(name=request.POST['select_group'])
+        
+    U=User.objects.filter(email=request.POST['email'])
+    if U.count>0:
+        U=User.objects.get(email=request.POST['email'])
+        i=Invite(groupId=grp_name,invitedBy=request.user,theInvitedOne=U)
+        i.save()    
+    else:
+             email_body = """
+You have been invited to Money Club!! :)
+You have an invite from """ +grp_name.name +"""
+""" 
+             send_mail(subject="Invite to a Money Club!",
+            message=email_body,
+              from_email="invites@grumblr.com",
+              recipient_list=[request.POST['email']])
+        
+    return render(request, 'moneyclub/adduser_success.html', {})
+ 
 
     
