d02e483d5816b480a3e42641bf8e8b39ac7ca43b
ernestwang <erzhuo.wang88@gmail.com>
2013-12-02 11:54:27 -0500

    some animation

--- moneyclub/static/js/animate.js
+++ moneyclub/static/js/animate.js
@@ -1,16 +1,51 @@
+$(document).ready(function(){
 // Animate the element's value from x to y:
-          $({ someValue: 0 }).animate({ someValue: parseInt($('.count').html()) }, {
-              duration: 3000,
+
+          var from = 0;
+          var to = parseInt($(".count").text());
+          $({count:from}).animate({count: to}, {
+              duration: 2500,
               easing: 'swing', // can be anything
+              
               step: function () { // called on every step
                   // Update the element's text with rounded-up value:
-                  $('.count').text(commaSeparateNumber(Math.round(this.someValue)));
+                  $('.count').text(commaSeparateNumber(Math.round(this.count)));
+                                      
               }
+
+
           });
 
           function commaSeparateNumber(val) {
+              console.log(val)
               while (/(\d+)(\d{3})/.test(val.toString())) {
                   val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
               }
               return val;
           }
+          
+
+  /*
+counts = {};
+ 
+      function format_number(text){
+          
+          return text.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+      };
+       
+      function magic_number(element_name, value) {
+          var elem = $(element_name);
+          var current = counts[element_name] || 0;
+          $({count: current}).animate({count: value}, {
+                                      duration: 2000,
+                                      easing: 'swing',
+                                      step: function() {
+                                        var text=format_number(String(parseInt(this.count)))
+                                        console.log(text)
+                                          elem.text(format_number(String(Math.round(parseInt(this.count)))));
+                                      }});
+                                      counts[element_name] = value;
+      };
+magic_number('.count', $('.count').text());
+*/
+})
