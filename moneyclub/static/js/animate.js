2556546678275d45a5ddd42ec9d6ea6c0339357f
ernestwang <erzhuo.wang88@gmail.com>
2013-12-02 12:00:53 -0500

    settings.py still

--- moneyclub/static/js/animate.js
+++ moneyclub/static/js/animate.js
@@ -17,7 +17,6 @@ $(document).ready(function(){
           });
 
           function commaSeparateNumber(val) {
-              console.log(val)
               while (/(\d+)(\d{3})/.test(val.toString())) {
                   val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
               }
@@ -25,27 +24,5 @@ $(document).ready(function(){
           }
           
 
-  /*
-counts = {};
- 
-      function format_number(text){
-          
-          return text.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-      };
-       
-      function magic_number(element_name, value) {
-          var elem = $(element_name);
-          var current = counts[element_name] || 0;
-          $({count: current}).animate({count: value}, {
-                                      duration: 2000,
-                                      easing: 'swing',
-                                      step: function() {
-                                        var text=format_number(String(parseInt(this.count)))
-                                        console.log(text)
-                                          elem.text(format_number(String(Math.round(parseInt(this.count)))));
-                                      }});
-                                      counts[element_name] = value;
-      };
-magic_number('.count', $('.count').text());
-*/
+
 })
